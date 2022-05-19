02d1ac16125e3c12492550c7c8d7d316a83e19fd
Luciana Zanetti <luciana.zeta@gmail.com>
2022-05-19 15:44:26 -0300

    computerChoice-userChoice

--- app.js
+++ app.js
@@ -8,6 +8,34 @@ const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 
+function getComputerChoice(){
+    const choices = [ 'r', 'p', 's'];
+    const randomNumber = (Math.floor(Math.random()* 3));  
+    return choices[randomNumber];
+}
+//console.log(getComputerChoice());
+
+
+function game(userChoice){
+ const computerChoice = getComputerChoice();
+ console.log("user choice =>" + userChoice);
+ console.log(" computerchoice =>" + computerChoice);
+};
+function main(){
 rock_div.addEventListener('click', function(){
-    console.log("hey, you clicked on rock");
+    //console.log("hey, you clicked on rock");
+    game("r");
 })
+
+paper_div.addEventListener('click', function(){
+   // console.log("hey, you clicked on paper");
+   game("p");
+})
+
+scissors_div.addEventListener('click', function(){
+   // console.log("hey, you clicked on scissors");
+   game("s");
+})
+}
+
+main();
